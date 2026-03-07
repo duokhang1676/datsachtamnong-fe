a527e785db13944bfefa6238f33a1ef2af7174a9
Võ Dương Khang <114740029+duokhang1676@users.noreply.github.com>
2026-03-07 21:22:04 +0700

    Fix: Remove extra whitespace in desktop admin layout caused by mobile responsive margins

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -191,7 +191,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         </aside>
 
         {/* Main Content */}
-        <main className="flex-1 lg:ml-64 p-4 lg:p-8 mt-16 lg:mt-0">{children}</main>
+        <main className="flex-1 p-4 lg:p-8 pt-20 lg:pt-8">{children}</main>
       </div>
     </div>
   );
